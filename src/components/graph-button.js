8a007d8da166e3f460540beaca76b0aac6aba8b9
nhsuh <nsuh813@gmail.com>
2024-01-06 19:07:05 -0500

    Abstracted Buttons

--- src/components/graph-button.js
+++ src/components/graph-button.js
@@ -2,15 +2,14 @@ class GraphButton extends HTMLElement {
   constructor() {
     super();
     this.innerHTML = `
-    <button
-    id="add-node"
-    class="text-2xl text-gray-300 px-12 mt-4 bg-cyan-800 hover:bg-cyan-700 rounded-xl"
-  >
-    Add Node
-  </button>
-        
+    <button class="text-2xl text-gray-300 px-12 mt-4 bg-cyan-800 hover:bg-cyan-700 rounded-xl">
+        Add Node
+    </button>
     `;
   }
-  connectedCallback() {}
+  connectedCallback() {
+    this.querySelector("button").textContent = this.getAttribute("label");
+    this.querySelector("button").id = this.getAttribute("id");
+  }
 }
 customElements.define("graph-button", GraphButton);
